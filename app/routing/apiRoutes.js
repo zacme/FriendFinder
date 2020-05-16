5cad24dfb2ec3e4f6319ed506cd9d83474880150
Zack Emerson <zacme429@gmail.com>
2020-05-15 18:35:07 -0600

    layout and questions are coded

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -31,7 +31,7 @@ module.exports = function(app) {
 		for (var i = 0; i < friends.length; i++) {
 			// console.log('friend = ' + JSON.stringify(friends[i]));
 
-			// Compute differenes for each question
+			// Compute differences for each question
 			var diff = 0;
 			for (var j = 0; j < userResponses.length; j++) {
 				diff += Math.abs(friends[i].scores[j] - userResponses[j]);
@@ -53,7 +53,6 @@ module.exports = function(app) {
 		// Add new user
 		friends.push(userInput);
 
-		// Send appropriate response
-		res.json({status: 'OK', matchName: matchName, matchImage: matchImage});
+	
 	});
 };
--- server.js
+++ server.js
@@ -1,39 +1,25 @@
-// ==============================================================================
-// DEPENDENCIES
-// Series of npm packages that we will use to give our server useful functionality
-// ==============================================================================
+// Pull in required dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
 
-var express = require("express");
-
-// ==============================================================================
-// EXPRESS CONFIGURATION
-// This sets up the basic properties for our express server
-// ==============================================================================
-
-// Tells node that we are creating an "express" server
+// Configure the Express application
 var app = express();
-
-// Sets an initial port. We"ll use this later in our listener
 var PORT = process.env.PORT || 8080;
 
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// Expose the public directory to access CSS files
+app.use(express.static(path.join(__dirname, './app/public')));
 
-// ================================================================================
-// ROUTER
-// The below points our server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-// ================================================================================
+// Add middleware for parsing incoming request bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+// Add the application routes
+require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
 
-// =============================================================================
-// LISTENER
-// The below code effectively "starts" our server
-// =============================================================================
-
+// Start listening on PORT
 app.listen(PORT, function() {
-  console.log("App listening on PORT: " + PORT);
+  console.log('Friend Finder app is listening on PORT: ' + PORT);
 });
